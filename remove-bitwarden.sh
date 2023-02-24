d38524e69a43842f6cdb205aafaee12f916646d8
devsecsols <124737084+devsecsols@users.noreply.github.com>
2023-02-24 23:50:36 +0000

    Update remove-bitwarden.sh

--- remove-bitwarden.sh
+++ remove-bitwarden.sh
@@ -6,3 +6,11 @@ docker kill $(docker ps -q --filter "name=bitwarden")
 # Remove all stopped containers with the name "bitwarden" in them
 docker rm $(docker ps -aq --filter "name=bitwarden")
 
+# Remove the "bitwarden.sh" file in the current user's home directory
+rm -f "$HOME/bitwarden/bitwarden.sh"
+
+# Remove the "bitwarden.sh" file in the current user's home directory
+rm -f "$HOME/bitwarden/remove-bitwarden.sh"
+
+# Delete the "bitwarden" directory in the current user's home directory
+rm -rf "$HOME/bitwarden"
